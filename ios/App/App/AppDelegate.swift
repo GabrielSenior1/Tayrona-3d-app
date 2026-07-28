59d13cd5e5902696df9db0f45cbfd3a2f4f80e22
GabrielSenior1 <87051149+GabrielSenior1@users.noreply.github.com>
2026-07-28 00:09:15 -0500

    Fix: Re-add manual plugin registration for ARLauncherPlugin in AppDelegate

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Capacitor
+import ArLauncher
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -8,6 +9,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if let bridgeViewController = self.window?.rootViewController as? CAPBridgeViewController {
+            bridgeViewController.bridge?.registerPluginInstance(ARLauncherPlugin())
+        }
         return true
     }
 
